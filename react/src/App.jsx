5d906e22984e127a3d60023ba2e42bdd2f9ad107
Douglas Mkonyi <fortunatusdouglas@gmail.com>
2024-10-23 10:02:07 +0300

    feat: adding bootstrap files

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,6 +1,9 @@
 import FormTable from "./Components/FormTable";
 import Logo from "./Components/Logo";
 import Sidebar from "./Components/Sidebar/Index";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
@@ -50,6 +53,33 @@ function App() {
             <input type="submit" value="Go" />
           </div>
         </form>
+      </div>
+
+      <div className="container my-3 border">
+        <h2>
+          Incase you want to use bootstrap <code>App.jsx</code> this is the code
+        </h2>
+
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
       </div>
     </>
   );
